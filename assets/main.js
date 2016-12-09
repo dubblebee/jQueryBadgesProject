a86599ce5fa83ca999cc2f4adecfdc143227e23f
Brian Benton <brianbenton77@gmail.com>
2016-12-09 14:58:29 +0000

    Code School solution

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,40 @@
 $(function() {
 
-  // your code will go here
-
   $.ajax({
     url: 'https://www.codeschool.com/users/2331517.json',
     dataType: 'jsonp',
-    success: function(response) {
-      // handle response
-
-      for (var i of response.courses.completed) {
-
-      	var badge = i.badge;
-      	var title = i.title;
-      	var url = i.url;
-      	
-      	$("#badges").append('\
-      		<div class="course">\
-      		    <h3>'+ title +'</h3>\
-      		    <img src="'+ badge +'"></img>\
-      		    <a href="'+ url +'" target="_blank" class="btn btn-primary">See Course</a>\
-      		</div>\
-      	');
-      }
-
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
     }
   });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
---
+++ assets/main.oldjs.js
@@ -0,0 +1,29 @@
+$(function() {
+
+  // your code will go here
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2331517.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response', response);
+
+      for (var i of response.courses.completed) {
+
+      	var badge = i.badge;
+      	var title = i.title;
+      	var url = i.url;
+      	
+      	$("#badges").append('\
+      		<div class="course">\
+      		    <h3>'+ title +'</h3>\
+      		    <img src="'+ badge +'"></img>\
+      		    <a href="'+ url +'" target="_blank" class="btn btn-primary">See Course</a>\
+      		</div>\
+      	');
+      }
+
+    }
+  });
+
+});
